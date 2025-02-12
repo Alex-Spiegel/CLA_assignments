bda1528d4531c1e6ca509adffd079b6b8bc3d747
Alex-Spiegel <alexander.schaz@gmail.comgit>
2025-02-12 19:20:08 +0100

    minor changes for assign 13

--- assignment_13/app/seed.js
+++ assignment_13/app/seed.js
@@ -1,19 +1,24 @@
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const User = require("./models/userModel");
+const User = require("./models/userSchema");
+
+dotenv.config({ path: "../.env" }); // Pfad nötig, da dotenv.config nur im VZ des skripts nach .env sucht
 
 // Dummy-Daten Funktion
 const seedDatabase = async () => {
   try {
     // Verbindung zur DB
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, { dbName: "CodeCLA-DB" }); // explizit sagen, sonst wird DB namens "test" erstellt
 
     // Dummy User (Manager)
     await User.create({
-      first_name: "Alice",
+      first_name: "Sohaib",
       last_name: "Admin",
-      email: "alice@example.com",
+      email: "sohaib@example.com",
       password: "securepassword",
-      role: "manager",
+      role: "coder",
+      description: "loves algorythms",
+      score: 300,
     });
 
     console.log("✅ Dummy-Daten erfolgreich hinzugefügt!");
